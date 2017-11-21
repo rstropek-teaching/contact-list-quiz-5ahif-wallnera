f4ed3348ef4a9a3acf4d55222d4e535519a1c698
Alexander Wallner <IF130031@HTBLAPerg.onmicrosoft.com>
2017-11-21 15:44:53 +0100

    Contact List Swagger Homework

---
+++ ContactListSwagger/ContactListSwagger/ContactsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactListSwagger
+{
+    [Route("/contacts")]
+    public class ContactController : Controller
+    {
+        public static List<Person> items = new List<Person>();
+        public int counterId=1;
+
+        public ContactController()
+        {
+            
+            items.Add(new Person(this.counterId, "Chuck", "Norris", "noemailneededforchucknorris"));
+            this.counterId++;
+            items.Add(new Person(this.counterId, "Obi-Wan", "Kenobi","obiwan@jedimaster.com"));
+            this.counterId++;
+            items.Add(new Person(this.counterId, "Anakin", "Skywalker", "aniwalker@nojedimaster.com"));
+        }
+
+        [HttpGet/*("/contacts")*/]
+        public IActionResult GetAll()
+        {
+            return Ok(items);
+        }
+
+        [HttpPost/*("/contacts")*/]
+        public IActionResult Add([FromBody] Person test)
+        {
+
+            if(test == null)
+            {
+                return BadRequest();
+            }
+            test.Id = this.counterId;
+            items.Add(test);
+            return CreatedAtRoute("contacts", test);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var pers = items.FirstOrDefault(t => t.Id == id);
+            if (pers == null)
+            {
+                return NotFound();
+            }
+
+            items.Remove(pers);
+            return Ok("Deleted Successfully!");
+        }
+
+        [HttpGet("findByName/{name}")]
+        public IActionResult Findbyname(string name)
+        {
+            if (name.Equals(""))
+            {
+                return BadRequest();
+            }
+            List<Person> sol = new List<Person>();
+            foreach(Person pers in items)
+            {
+                if (pers.Firstname.ToLower().Contains(name.ToLower()) || pers.Lastname.ToLower().Contains(name.ToLower()))
+                {
+                    sol.Add(pers);
+                }
+            }
+            if (!sol.Any())
+            {
+                return Ok("Nothing found!");
+            }
+            return Ok(sol);
+        }
+    }
+}
---
+++ ContactListSwagger/ContactListSwagger/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactListSwagger
+{
+    public class Person
+    {
+
+        public Person(int id, string firstname, string lastname, string email)
+        {
+            this.Id = id;
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Email = email;
+        }
+
+        public Person()
+        {
+            this.Id = 0;
+            this.Firstname = "unknown";
+            this.Lastname = "unknown";
+            this.Email = "unknown";
+        }
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+
+
+        public string FormatPersonToString()
+        {
+            string personserial = "";
+
+            personserial += this.Id;
+            personserial += this.Firstname;
+            personserial += this.Lastname;
+            personserial += this.Email;
+
+            return personserial;
+        }
+    }
+}
